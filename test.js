c007bc58b1aac18ee7c674070be4670a5c8ebab9
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-08-21 16:15:27 +0800

    Clean workspace

--- test.js
+++
@@ -1,5 +0,0 @@
-let str = "1+2/3*4-1";
-
-let numbersInExpression = str.split('+').join(', ').split('-').join(', ').split('*').join(', ').split('/').join(', ').split(', ');
-
-console.log(numbersInExpression);
